f748eced2883a0b7c5bcecbec3b3064e215a9ca3
lmmj73 <35739236+lmmj73@users.noreply.github.com>
2023-12-04 15:49:51 +0000

    Update project.py
    reintegrated rest api

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import serial
 from ssp import ssp, ser
 from ssp_device import ssp_device
-#import rest_api
+import rest_api
 
 
 ssp_device.sync(ssp_device)
@@ -24,5 +24,5 @@ while True:
 
     ssp_device.poll(ssp_device)
 
-#   rest_api.rest_api().post()
+    rest_api.rest_api().post()
 
